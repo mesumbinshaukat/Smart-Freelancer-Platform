863bff2c24ede188d037b4cdb244e9a9d301352b
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-07-18 22:07:59 +0500

    Updated

--- connection/connection.php
+++ connection/connection.php
@@ -1,2 +1,13 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "db_freelance", 3306) or die("Connection Failed");
+require_once realpath(__DIR__ . '/../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__ . '/..'));
+$dotenv->load();
+
+$localhost = $_ENV['localhost'];
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+$database_name = $_ENV['database_name'];
+$port = $_ENV['port'];
+
+$con = mysqli_connect($localhost, $username, $password, $database_name, $port) or die("Connection Failed");
