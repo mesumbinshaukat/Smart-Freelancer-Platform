d01593d9494f8776c3d93008f741841bebe8f5f1
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-07-22 10:41:28 +0500

    Updated Database

--- db_freelance.sql
+++ db_freelance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2024 at 05:57 PM
+-- Generation Time: Jul 22, 2024 at 07:40 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
